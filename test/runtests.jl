32d074e2ca370658909e4cdc604119e36455be4a
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2020-03-26 20:33:24 +0100

    More unit tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,12 +4,12 @@ using LinearAlgebra
 using Test
 
 @testset "ThreadedSparseArrays.jl" begin
-    M,N = 5000,4000
+    N = 4000
     n = 200
     T = ComplexF64
 
     C = sprand(T, N, n, 0.05)
-    @testset "$(Mat)" for Mat in [ThreadedSparseMatrixCSC, ThreadedColumnizedSparseMatrix]
+    @testset "$(Mat)_R" for Mat in [ThreadedSparseMatrixCSC, ThreadedColumnizedSparseMatrix]
         Ct = Mat(C)
 
         eye = Matrix(one(T)*I, N, N)
@@ -18,4 +18,25 @@ using Test
         ref = eye*C
         @test norm(ref-out) == 0
     end
+
+    @testset "$(Mat)_L" for Mat in [ThreadedSparseMatrixCSC]
+        Ct = Mat(C)
+
+        eye = Matrix(one(T)*I, n, n)
+        out = zeros(T, N, n)
+        LinearAlgebra.mul!(out, Ct, eye)
+        ref = C*eye
+        @test norm(ref-out) == 0
+    end
+
+    @testset "$(Mat)_L_$(op)" for op in [adjoint,transpose], Mat in [ThreadedSparseMatrixCSC]
+        Ct = Mat(C)
+
+        eye = Matrix(one(T)*I, N, N)
+        out = zeros(T, n, N)
+        LinearAlgebra.mul!(out, op(Ct), eye)
+        ref = op(C)*eye
+        @test norm(ref-out) == 0
+    end
+
 end
