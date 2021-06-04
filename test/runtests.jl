2d6e03dcfcdb919a07f9c9c3e7104ec7b788b5aa
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2021-06-04 19:51:27 +0200

    Unit tests to test which mul! cases are threaded
    Also some cleanup

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,14 +3,28 @@ using SparseArrays
 using LinearAlgebra
 using Test
 
+
+function match_exception(f, ::Type{T}=DimensionMismatch, func=:mul!, path="ThreadedSparseArrays/src/ThreadedSparseArrays.jl") where T
+    try
+        f()
+    catch ex
+        st = stacktrace(catch_backtrace())[1]
+        p = splitpath(path)
+        p2 = splitpath(string(st.file))
+        return ex isa T && st.func == func && p==p2[max(1,end-length(p)+1):end]
+    end
+    false
+end
+
+
 @testset "ThreadedSparseArrays.jl" begin
     N = 4000
     n = 200
     T = ComplexF64
 
     C = sprand(T, N, n, 0.05)
-    @testset "$(Mat)_R" for Mat in [ThreadedSparseMatrixCSC]
-        Ct = Mat(C)
+    @testset "R" begin
+        Ct = ThreadedSparseMatrixCSC(C)
 
         eye = Matrix(one(T)*I, N, N)
         out = zeros(T, N, n)
@@ -19,8 +33,8 @@ using Test
         @test norm(ref-out) == 0
     end
 
-    @testset "$(Mat)_L" for Mat in [ThreadedSparseMatrixCSC]
-        Ct = Mat(C)
+    @testset "L" begin
+        Ct = ThreadedSparseMatrixCSC(C)
 
         eye = Matrix(one(T)*I, n, n)
         out = zeros(T, N, n)
@@ -29,8 +43,8 @@ using Test
         @test norm(ref-out) == 0
     end
 
-    @testset "$(Mat)_L_$(op)" for op in [adjoint,transpose], Mat in [ThreadedSparseMatrixCSC]
-        Ct = Mat(C)
+    @testset "L_$(op)" for op in [adjoint,transpose]
+        Ct = ThreadedSparseMatrixCSC(C)
 
         eye = Matrix(one(T)*I, N, N)
         out = zeros(T, n, N)
@@ -40,8 +54,8 @@ using Test
     end
 
     x = rand(0:1,N)
-    @testset "$(Mat)_L_$(op)_vec" for op in [adjoint,transpose], Mat in [ThreadedSparseMatrixCSC]
-        Ct = Mat(C)
+    @testset "L_$(op)_vec" for op in [adjoint,transpose]
+        Ct = ThreadedSparseMatrixCSC(C)
 
         out = zeros(T, n)
         LinearAlgebra.mul!(out, op(Ct), x)
@@ -50,11 +64,26 @@ using Test
     end
 
 
+    # Test that all combinations of dense*sparse multiplication are threaded
+    @testset "Threading" begin
+        A = ThreadedSparseMatrixCSC(spzeros(2,3))
+        B = zeros(4,1)
+        @test match_exception(()->A*B)
+        @test match_exception(()->A'B)
+        @test match_exception(()->A*B')
+        @test match_exception(()->A'B')
+        @test match_exception(()->B*A)
+        @test match_exception(()->B'A)
+        # @test match_exception(()->B*A') # TODO: implement!
+        # @test match_exception(()->B'A') # TODO: implement!
+    end
+
+
     # These test below are here to ensure we don't hit ambiguity warnings.
     # The implementations are not (currently) threaded.
     sx = sprand(Bool,n,0.05)
-    @testset "$(Mat)_L_sparsevec" for Mat in [ThreadedSparseMatrixCSC]
-        Ct = Mat(C)
+    @testset "L_sparsevec" begin
+        Ct = ThreadedSparseMatrixCSC(C)
 
         out = Ct*sx
         ref = C*sx
@@ -63,8 +92,8 @@ using Test
     end
 
     sx = sprand(Bool,N,0.05)
-    @testset "$(Mat)_L_$(op)_sparsevec" for op in [adjoint,transpose], Mat in [ThreadedSparseMatrixCSC]
-        Ct = Mat(C)
+    @testset "L_$(op)_sparsevec" for op in [adjoint,transpose]
+        Ct = ThreadedSparseMatrixCSC(C)
 
         out = op(Ct)*sx
         ref = op(C)*sx
@@ -73,8 +102,8 @@ using Test
     end
 
     sx = sparse(rand(1:n,10),1:10,true,n,10)
-    @testset "$(Mat)_L_sparse" for Mat in [ThreadedSparseMatrixCSC]
-        Ct = Mat(C)
+    @testset "L_sparse" begin
+        Ct = ThreadedSparseMatrixCSC(C)
 
         out = Ct*sx
         ref = C*sx
@@ -83,13 +112,12 @@ using Test
     end
 
     sx = sparse(rand(1:N,10),1:10,true,N,10)
-    @testset "$(Mat)_L_$(op)_sparse" for op in [adjoint,transpose], Mat in [ThreadedSparseMatrixCSC]
-        Ct = Mat(C)
+    @testset "L_$(op)_sparse" for op in [adjoint,transpose]
+        Ct = ThreadedSparseMatrixCSC(C)
 
         out = op(Ct)*sx
         ref = op(C)*sx
         @test norm(ref-out) == 0
         @test typeof(ref)==typeof(out)
     end
-
 end
